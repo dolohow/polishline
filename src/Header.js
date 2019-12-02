da30bedb2c8db54d212d7657e84db06e554f515c
Łukasz Żarnowiecki <lukasz@zarnowiecki.pl>
2019-12-02 23:27:36 +0100

    Add menu

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 
 import './Header.scss';
 
 class Header extends React.Component {
+    constructor() {
+        super();
+        this.state = { showMenu: false };
+    }
+    toggleMenu = () => {
+        this.setState({ showMenu: !this.state.showMenu })
+    }
+
     render() {
         return (
-            <header class="Header">
-                <div class="logo">
-                    <img src="mountains.svg"></img>
+            <header className="Header">
+                <div className="bar">
+                    <div className="logo">
+                        <img src="mountains.svg"></img>
+                    </div>
+                    <CSSTransition timeout={{exit: 200}} classNames="hamburger" in={this.state.showMenu}>
+                        <div className="hamburger" onClick={this.toggleMenu} >
+                            <img src="hamburger.svg"></img>
+                        </div></CSSTransition>
+                    <div className="search">
+                        <img src="magnifier.svg"></img>
+                    </div>
                 </div>
-                <div class="hamburger">
-                    <img src="hamburger.svg"></img>
-                </div>
-                <div className="search">
-                    <img src="magnifier.svg"></img>
-                </div>
+                <CSSTransition classNames="menu" in={this.state.showMenu} timeout={200} unmountOnExit>
+                    <div className="menu">
+                        <ul>
+                            <li>Autorzy</li>
+                            <li>Galeria</li>
+                            <li>Facebook</li>
+                        </ul>
+                    </div>
+                </CSSTransition>
             </header>
         )
     }
