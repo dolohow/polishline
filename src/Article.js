bc82f1a47860e33a375f54daa69da442c5bc162f
Łukasz Żarnowiecki <lukasz@zarnowiecki.pl>
2019-12-02 15:40:45 +0100

    Add Header

--- src/Article.js
+++ src/Article.js
@@ -10,18 +10,15 @@ function formatDate(date) {
 class Article extends React.Component {
     render() {
         const styles = {
-            background: `url(${this.props.img}) no-repeat`,
-            backgroundPosition: 'center center',
-            backgroundSize: 'cover',
-            height: '230px',
+            background: `url(${this.props.img}) center center / cover no-repeat`,
         };
         return (
             <article className="Article">
                 <a href="#">
-                    <div style={styles}></div>
+                    <div className="image" style={styles}></div>
                     <div className="article-content">
                         <div className="author">{this.props.author}</div>
-                        <div className="title">{this.props.title}</div>
+                        <div className="title" dangerouslySetInnerHTML={{__html: this.props.title}}></div>
                         <div className="excerpt" dangerouslySetInnerHTML={{__html: this.props.excerpt}}></div>
                         <hr />
                         <div className="date">{formatDate(this.props.date)}</div>
--- src/Header.js
+++ src/Header.js
@@ -6,10 +6,16 @@ import './Header.scss';
 class Header extends React.Component {
     render() {
         return (
-            <header>
-                <Link to="/"></Link>
-                <div className="Header-toggle-menu"></div>
-                <div className="Header-styles-search"></div>
+            <header class="Header">
+                <div class="logo">
+                    <img src="mountains.svg"></img>
+                </div>
+                <div class="hamburger">
+                    <img src="hamburger.svg"></img>
+                </div>
+                <div className="search">
+                    <img src="magnifier.svg"></img>
+                </div>
             </header>
         )
     }
