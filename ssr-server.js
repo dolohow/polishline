2b91b6d05f6f5ed532c6c585f2b1dd177bfcd21f
Łukasz Żarnowiecki <lukasz@zarnowiecki.pl>
2020-04-03 12:55:48 +0200

    Add SSR server

---
+++ ssr-server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const rendertron = require('rendertron-middleware');
+
+const app = express();
+
+app.use(rendertron.makeMiddleware({
+  proxyUrl: 'https://render-tron.appspot.com/render',
+}));
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(8080);
