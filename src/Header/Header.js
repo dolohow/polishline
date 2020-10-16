bde3048411bd367466fd9015d2f3815e1ce95286
Łukasz Żarnowiecki <lukasz@zarnowiecki.pl>
2020-10-16 19:23:45 +0200

    Header: Add real links

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { useQuery, gql } from '@apollo/client';
 
 import Search from './Search';
@@ -13,6 +13,7 @@ const GET_MENU_ITEMS = gql`
             menuItems {
                 nodes {
                     label
+                    url
                 }
             }
           }
@@ -21,80 +22,78 @@ const GET_MENU_ITEMS = gql`
 `;
 
 function Header() {
-    const header = useRef(null)
-    const [showMenu, setMenuState] = useState(false);
-    const [showMenuFromSearch, setShowMenuFromSearch] = useState(false);
-    const { loading, data } = useQuery(GET_MENU_ITEMS);
+  const header = useRef(null)
+  const [showMenu, setMenuState] = useState(false);
+  const { loading, data } = useQuery(GET_MENU_ITEMS);
 
 
-    useEffect(() => {
-        let prevScrollPos = window.pageYOffset;
+  useEffect(() => {
+    let prevScrollPos = window.pageYOffset;
 
-        const handleScroll = () => {
-            const currentScrollPos = window.pageYOffset;
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
 
-            if (showMenu) {
-                return;
-            }
+      if (showMenu) {
+        return;
+      }
 
-            if (prevScrollPos > currentScrollPos) {
-                header.current.classList.remove('Header-hide');
-            } else {
-                header.current.classList.add('Header-hide');
-            }
+      if (prevScrollPos > currentScrollPos) {
+        header.current.classList.remove('Header-hide');
+      } else {
+        header.current.classList.add('Header-hide');
+      }
 
-            prevScrollPos = currentScrollPos;
-        };
+      prevScrollPos = currentScrollPos;
+    };
 
-        window.addEventListener('scroll', handleScroll, true);
-        return () => window.removeEventListener('scroll', handleScroll);
-    });
+    window.addEventListener('scroll', handleScroll, true);
+    return () => window.removeEventListener('scroll', handleScroll);
+  });
 
-    if (loading) return <></>;
+  if (loading) return <></>;
 
-    const closeMenu = () => {
-        setMenuState(false);
-        setShowMenuFromSearch(false);
-    }
+  const closeMenu = () => {
+    setMenuState(false);
+  }
 
-    const toggleMenu = () => {
-        setMenuState(!showMenu);
-    }
+  const toggleMenu = () => {
+    setMenuState(!showMenu);
+  }
 
-    const toggleMenuFromSearch = () => {
-        toggleMenu();
-        setShowMenuFromSearch(!showMenuFromSearch);
-    }
-
-    return (
-        <header>
-            <div ref={header} className="Header">
-                <div className="logo">
-                    <Link onClick={closeMenu} to="/">
-                        <img alt="logo" src="/logo.png"></img>
-                    </Link>
-                </div>
-                <div className={`hamburger ${showMenu ? "change" : ""}`} onClick={toggleMenu}>
-                    <div className="bar1"></div>
-                    <div className="bar2"></div>
-                    <div className="bar3"></div>
-                </div>
-                <div onClick={toggleMenuFromSearch} className="search">
-                    <img alt="szukaj" src="/magnifier.svg"></img>
-                </div>
-                <div className={`menu ${showMenu ? 'menu-show' : ""}`}>
-                    <Search onLinkClicked={closeMenu} focus={showMenuFromSearch} />
-                    <div className="links">
-                        <ul>
-                            {data.menus.nodes[0].menuItems.nodes.map((val, key) =>
-                                <li key={key}>{val.label}</li>
-                            )}
-                        </ul>
-                    </div>
-                </div>
+  return (
+    <header>
+      <div ref={header} className="Header">
+        <div className="logo">
+          <Link onClick={closeMenu} to="/">
+            <img alt="logo" src="/logo.png"></img>
+          </Link>
+        </div>
+        <div className={`hamburger ${showMenu ? "change" : ""}`} onClick={toggleMenu}>
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
+        </div>
+        <div className="search">
+          <img alt="koszyk" src="/shopping-cart.svg"></img>
+        </div>
+        <div className={`menu ${showMenu ? 'menu-show' : ""}`}>
+          <Search onLinkClicked={closeMenu} />
+          {data.menus.nodes[0]?.menuItems ?
+            <div className="links">
+              <ul>
+                {data.menus.nodes[0].menuItems.nodes.map((val, key) =>
+                  <li key={key}>
+                    <Link onClick={closeMenu} to={val.url}>{val.label}</Link>
+                  </li>
+                )}
+              </ul>
             </div>
-        </header>
-    )
+            : null
+          }
+        </div>
+      </div>
+    </header>
+  )
 }
 
 
